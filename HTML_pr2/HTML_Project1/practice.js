68ef3b6a032f0cd1b4dee6af8a9732d31dfc3cbc
Your Name <guravmadhuri93@gmail.com>
2024-11-28 11:35:39 +0530

    first html file added

---
+++ HTML_pr2/HTML_Project1/practice.js
@@ -0,0 +1,8 @@
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World"
+}
+function openWindow() {
+  document.getElementById("openwindow").innerHTML = "Hello World"
+}
+</script>
